bcdbf0277217805c75c6afcd4fbb837ecc29d1f4
Samuel Kaiser <samuel.kaiser01@gmail.com>
2020-01-20 16:16:39 +0100

    Improve html page

--- build.js
+++ build.js
@@ -12,10 +12,34 @@ writeFileSync('docs/index.html', `
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Highlight</title>
+    <style>
+      body {
+        margin: 1.5rem;
+        font-family: sans-serif;
+      }
+      a {
+        font-size: 1.2rem;
+      }
+    </style>
   </head>
   <body>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, vero magnam obcaecati tempora quae voluptatem fugiat maxime voluptatibus debitis, consectetur odit. Nostrum ipsa doloribus totam omnis enim harum quo labore.</p>
-    <a href="javascript:${minified.replace(/"/g, "&quot;")}">Highlight!</a>
+    <a href="javascript:${minified.replace(/"/g, "&quot;")}">Highlight</a>
+    <p>
+      <span>Click the link above and try selecting </span>
+      <span style="background: yellow">some text</span><span>. Double-click again to deselect.</span>
+    </p>
+    <p>
+      <span>To use this on any other website, save the link as a bookmark by </span>
+      <span style="background: yellow">dragging it into your bookmark bar</span><span>.</span>
+    </p>
+    <p><small>Try pressing CTRL + Shift + B if you cannot find a bookmark bar</small></p>
+    <p><small>Please note that this tool is very buggy at this point. But in its very core function, it works. You might want to check back later for updates.</small></p>
+    <hr>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, vero magnam obcaecati tempora quae voluptatem fugiat maxime voluptatibus debitis, consectetur odit. Nostrum ipsa doloribus totam omnis enim harum quo labore.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, vero magnam obcaecati tempora quae voluptatem fugiat maxime voluptatibus debitis, consectetur odit. Nostrum ipsa doloribus totam omnis enim harum quo labore.
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint, vero magnam obcaecati tempora quae voluptatem fugiat maxime voluptatibus debitis, consectetur odit. Nostrum ipsa doloribus totam omnis enim harum quo labore.
+    </p>
   </body>
 </html>
 `)
